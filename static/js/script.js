20f80b2b4a91a2e4f59ada9d3c4b2eb3af7ff330
patataman <100316890@alumnos.uc3m.es>
2024-09-18 19:37:01 +0200

    draw item when added

--- static/js/script.js
+++ static/js/script.js
@@ -35,6 +35,9 @@ async function verifyToken() {
 function convertToId(chatLink) {
     // Took from https://wiki.guildwars2.com/wiki/Talk:Chat_link_format
     // Thank you very much. I simplified it to only return id
+    if (!chatLink.trim()) {
+        return "noitem";
+    }
     const clean_chatLink = chatLink.slice(2,-1);
     const base64Decoded = atob(clean_chatLink).split("");
     const hexCodes = base64Decoded.map(c => "0x" + c.charCodeAt(0).toString(16))
@@ -52,11 +55,24 @@ async function search() {
 /////////////////////////////////
 /////////////////////////////////
 
+function _alertGenerator(text, color) {
+    /* Template for alerts
+    */
+    $("#alert-header").html("");
+    let html = `
+      <div class="alert alert-`+color+` d-flex align-items-center alert-dismissible fade show" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+        <div>
+    ` + text + `
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </div>`;
+    $("#alert-header").append(html);
+}
 
 async function isUserLogged() {
-    /*
-     * Get and verify user token is exists
-     **/
+    /* Get and verify user token is exists
+     */
     let storage = window.localStorage;
     if (!storage.getItem("token")) {
         console.log("user not logged");
@@ -70,30 +86,7 @@ async function isUserLogged() {
     return true;
 }
 
-function getUserData() {
-    /*
-     * Returns all the user data stored in the storage
-     *
-     **/
-    let storage = window.localStorage;
-}
-
-function _alertGenerator(text, color) {
-    /* Template for alerts
-    */
-    $("#alert-header").html("");
-    let html = `
-      <div class="alert alert-`+color+` d-flex align-items-center alert-dismissible fade show" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-        <div>
-    ` + text + `
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-      </div>`
-    return html;
-}
-
-async function submitUserToken () {
+async function submitUserToken() {
     /* Store user token in storage, and
      * verify it
     */
@@ -109,11 +102,87 @@ async function submitUserToken () {
         // Bad token, show alert
         console.log("bad token");
         storage.removeItem("token");
-        let token_alert = _alertGenerator(msg, "danger");
-        $("#alert-header").append(token_alert);
+        _alertGenerator(msg, "danger");
     }
 }
 
+function drawItem(item_json){
+    /* Paint the item in the todo-list according to user's
+     * api key.
+     * A bit transparent if not unlocked, no transparency if unlocked
+     * TODO Also draw related items if it can be crafted
+     * TODO Wiki link
+    */
+    console.log(item_json);
+    let _url = item_json['icon'];
+    let _name = item_json['name'];
+    let _rarity = item_json['rarity'];
+    // TODO check if item unlocked
+    let html = `
+    <div class="row d-flex align-items-center mb-4">
+      <!-- item image -->
+      <div class="col-auto">
+        <img src="`+_url+`" alt="`+_name+` icon">
+      </div>
+      <!-- related info -->
+      <div class="col-auto">
+        <h4 class="col-12 align-self-start">
+          `+_name+`
+        </h4>
+        <div class="row">
+          <div class="col-auto">
+            Thing 1
+          </div>
+          <div class="col-auto">
+            Thing 2
+          </div>
+        </div>
+      </div>
+    </div>`
+    $("#item-grid").append(html);
+
+}
+
+function addItem() {
+    /* Read item chat link from the input text, add it to localStorage
+     * and paint it in the todo-list
+     */
+    let storage = window.localStorage;
+    let registered_items = storage.getItem("items");
+    let input_val = $("#item-input-id").val();
+    let item_id = convertToId(input_val);
+
+    if (!registered_items)
+        registered_items = []
+    else
+        registered_items = registered_items.split(",").map((x) => parseInt(x));
+
+    if (registered_items && registered_items.includes(item_id)) {
+        console.log("alert");
+        _alertGenerator("Item already in TODO List", "warning");
+    } else {
+        search().then(function(resp){
+            registered_items.push(item_id);
+            storage.setItem("items", registered_items);
+            drawItem(resp);
+        });
+    }
+
+}
+
+function getUserData() {
+    /*
+     * Returns all the user data stored in the storage
+     *
+     **/
+    let storage = window.localStorage;
+    // TODO Draw user items
+}
+
+
+
+
+/// NAVBAR FUNCTIONS
 function resetToken() {
     /* User wants to change token
     */
@@ -122,6 +191,7 @@ function resetToken() {
     $("#input_apikey").val("");
     $("#loginModal").modal("show");
 }
+////////////////////
 
 
 function offsetNav() {
@@ -145,6 +215,11 @@ $(document).ready(function() {
     $("#loginModal button").on("click", submitUserToken);
     $("#resetToken").on("click", resetToken);
     $("#item-input-id").on("click", function(){ this.select(); });
+    $("#addItem-addon").on("click", addItem);
+    $("#item-input-id").on("keydown", function(e){
+        console.log("keydown");
+        if (e.keyCode == 13) { console.log("enter"); addItem(); }
+    });
 
     // Check if user has a session with valid token
     isUserLogged().then(function(userLogged){
